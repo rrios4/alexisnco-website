62f9c9c7d657dbc202c8105489941aee9bfec4eb
Rogelio Rios <rogeliorios348@gmail.com>
2022-12-24 11:27:59 -0600

    Made project public to fix urls on images

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<LayoutProps> = (props) => {
     const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-        {colorMode === 'light' ? <Global styles={css`body { background-image: url("https://raw.githubusercontent.com/rrios4/alexisnco-website/master/src/assets/alexisnco-background-design-1-back.png?token=GHSAT0AAAAAAB4QVE2FUU65QM2GJVNRCE3YY5HGNCA"); background-size: contain; !important; }`}/> : <Global styles={css`body { background-image: url("https://raw.githubusercontent.com/rrios4/alexisnco-website/master/src/assets/alexisnco-background-design-1-skin.png?token=GHSAT0AAAAAAB4QVE2FPBUMX26BPKGJHQWUY5HGMQQ"); background-size: contain; !important; }`}/>}
+        {colorMode === 'light' ? <Global styles={css`body { background-image: url("https://raw.githubusercontent.com/rrios4/alexisnco-website/master/src/assets/alexisnco-background-design-1-back.png"); background-size: contain; !important; }`}/> : <Global styles={css`body { background-image: url("https://raw.githubusercontent.com/rrios4/alexisnco-website/master/src/assets/alexisnco-background-design-1-skin.png"); background-size: contain; !important; }`}/>}
         <Nav/>
         <Flex flexDir={'column'} maxW={'full'}>
             <Flex mt={{base: '4rem', md: '0'}}>
