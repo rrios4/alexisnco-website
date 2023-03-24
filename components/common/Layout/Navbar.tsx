ad7c7a4b33fde6ea1b1930c6e857df524a55b92a
Rogelio Rios <rogeliorios348@gmail.com>
2023-03-24 11:47:22 -0500

    Updated transition duration

--- components/common/Layout/Navbar.tsx
+++ components/common/Layout/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
     }
     
     return (
-        <nav className={`bg-blue-700 bg-opacity-80 backdrop-blur-md w-full z-30 sticky shadow-md border-gray-100 transition duration-150 ease-in-out ${visible ? 'top-0 translate-y-[0px]' : '-translate-y-[100px]'}`}>
+        <nav className={`bg-blue-700 bg-opacity-80 backdrop-blur-md w-full z-30 sticky shadow-md border-gray-100 transition duration-300 ease-in-out ${visible ? 'top-0 translate-y-[0px]' : '-translate-y-[100px]'}`}>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
