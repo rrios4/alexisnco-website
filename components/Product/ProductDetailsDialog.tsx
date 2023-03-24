ed153741aeb9a9be9011942c2fe63aa45a154074
Rogelio Rios <rogeliorios348@gmail.com>
2023-03-24 04:01:28 -0500

    Updated z-index for product dialog

--- components/Product/ProductDetailsDialog.tsx
+++ components/Product/ProductDetailsDialog.tsx
@@ -17,7 +17,7 @@ const ProductDetailsDialog: React.FC<IProductDetailsDialogProps> = (props) => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className='bg-black bg-opacity-40 data-[state=open]:animate-overlayShow fixed inset-0'/>
-        <Dialog.Content className='text-white data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-blue-600 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'>
+        <Dialog.Content className='text-white z-40 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-blue-600 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'>
           {/* Modal Title with close button */}
           <div className='flex justify-between'>
             <div className='p-2 w-[15px] h-[15px]'>
